c6e8963829bdc322c96c38d0a5689e1e514b5fd3
haidong <1742482320@qq.com>
2019-07-04 22:57:02 +0800

    site 1 :site-wrapper site-header

---
+++ src/components/Site/SiteWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="page">
+        <div class="page-main">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "site-wrapper"
+}
+</script>
+
+<style scoped>
+    .page {
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-direction: column;
+        flex-direction: column;
+        -ms-flex-pack: column;
+        justify-content: center;
+        min-height: 100%;
+    }
+    .page-main {
+        -ms-flex: 1 1 auto;
+        flex: 11 auto;
+    }
+</style>
