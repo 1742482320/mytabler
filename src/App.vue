df6bc31b61676158f8035aa9668af269f36cb9c2
haidong <1742482320@qq.com>
2019-07-06 00:11:25 +0800

    css 1 :bundle.scss,veriable.scss

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
     <site-wrapper>
         <site-header imageUrl="" @toggle-menu="toggleMenu">
           <div class="nav-item">
-            <t-button :outline="true" size="sm" herf="https://github.com/tabler/tabler-vue" target="_blank">Source</t-button>
+            <!-- <t-button :outline="true" size="sm" herf="https://github.com/tabler/tabler-vue" target="_blank">Source</t-button> -->
           </div>
         </site-header>
     </site-wrapper>
 </template>
 
 <script>
+import SiteWrapper from "@/components/Site/SiteWrapper.vue"
+import SiteHeader from "@/components/Site/SiteHeader.vue"
 
 export default {
   name: 'app',
@@ -25,4 +27,5 @@ export default {
 
 <style lang="scss">
   // @import "./style/reboot.scss";
+  @import "./style/mytabler.scss"
 </style>
--- src/components/Site/SiteWrapper.vue
+++ src/components/Site/SiteWrapper.vue
@@ -13,17 +13,5 @@ export default {
 </script>
 
 <style scoped>
-    .page {
-        display: -ms-flexbox;
-        display: flex;
-        -ms-flex-direction: column;
-        flex-direction: column;
-        -ms-flex-pack: column;
-        justify-content: center;
-        min-height: 100%;
-    }
-    .page-main {
-        -ms-flex: 1 1 auto;
-        flex: 11 auto;
-    }
+
 </style>
